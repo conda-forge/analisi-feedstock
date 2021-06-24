27cbd45eeccc353117d9a12e62fd3b0b99a71f08
Riccardo Bertossa <riccardo.bertossa@gmail.com>
2021-06-25 00:00:28 +0200

    added mac build, more useful python package

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,14 @@ cmake ../ -DPYTHON_EXECUTABLE="$PYTHON" $ARGS
 make
 make test
 cp -v analisi "$PREFIX/bin/analisi_serial"
-cp -v pyanalisi*.so "$SP_DIR/"
+mkdir -p "$SP_DIR/pyanalisi"
+cp -v pyanalisi*.so "$SP_DIR/pyanalisi"
+cp -v ../notebooks/common.py "$SP_DIR/pyanalisi"
+echo 'from pyanalisi.pyanalisi import *
+from pyanalisi.common import *
+' > "$SP_DIR/pyanalisi/__init__.py"
+cp -v ../tools/cp2analisi.py "$PREFIX/bin/cp2analisi"
+cp -v ../tools/lammps2analisi.py "$PREFIX/bin/lammps2analisi"
 
 cd ../
 mkdir build_mpi
