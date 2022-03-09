c819a8460de44f3625a6d2e51c9f91eb1c4c505b
Riccardo Bertossa <riccardo.bertossa@gmail.com>
2022-03-09 11:20:47 +0100

    try to windows

---
+++ recipe/bld.bat
@@ -0,0 +1,25 @@
+@echo ON
+setlocal enabledelayedexpansion
+
+mkdir build 
+cd build
+
+cmake ../ -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      -DBUILD_MMAP=OFF -DBUILD_TESTS=OFF -DSYSTEM_XDRFILE=ON ^
+      -DSYSTEM_FFTW3=ON -DSYSTEM_EIGEN3=ON -DSYSTEM_BOOST=ON 
+
+if errorlevel 1 exit 1
+
+:: Build!
+nmake
+if errorlevel 1 exit 1
+
+:: Install!
+mkdir %SP_DIR%\pyanalisi
+cp pyanalisi* %SP_DIR%\pyanalisi\
+cp ..\pyanalisi\common.py %SP_DIR%\pyanalisi\
+cp ..\pyanalisi\__init__.py %SP_DIR%\pyanalisi\
+if errorlevel 1 exit 1
